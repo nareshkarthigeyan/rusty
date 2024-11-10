19d07f9811a333bb8df517414da7320f10af10e0
Naresh Karthigeyan <nareshkarthigeyan.2005@gmail.com>
2024-11-10 22:11:14 +0530

    impl init

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,11 @@ include!("user.rs");
 
 fn main() {
     let name = String::from("Naresh Karthigeyan");
-    let mut us = create_user(name);
-    display_user(&mut us);
-    display_user(&mut us);
+    let mut us = User::create_user(name);
+    println!("");
+    us.display_user();  
+    us.change_username(String::from("nareshkarthigeyan_2"));
+    println!("\nAfter username change\n");
+    us.display_user();
+    println!();
 }
--- src/user.rs
+++ src/user.rs
@@ -6,18 +6,23 @@ struct User {
     sign_in_count: u64,
 }
 
-fn create_user(nam: String) -> User {
-    let user1 = User {
-        active: true,
-        name: nam,
-        username: String::from("nareshkarthigeyan"),
-        email: String::from("nareshkarthigeyan.2005@gmail.com"),
-        sign_in_count: 1,
-    };
-    return user1;
-}
+impl User {
+    fn create_user(nam: String) -> User {
+        User {
+            active: true,
+            name: nam,
+            username: String::from("nareshkarthigeyan"),
+            email: String::from("nareshkarthigeyan.2005@gmail.com"),
+            sign_in_count: 1,
+        }
+    }
+    
+    fn display_user(&mut self) {
+        println!("Name: {}\nUsername: {}\nE-mail: {}\nSign In Count: {}\nUser Active?: {}", self.name, self.username, self.email, self.sign_in_count, self.active);
+        self.sign_in_count += 1;
+    }
 
-fn display_user(u: &mut User) {
-    println!("Name: {}\nUsername: {}\nE-mail: {}\nSign In Count: {}\nUser Active?: {}", u.name, u.username, u.email, u.sign_in_count, u.active);
-    u.sign_in_count += 1;
+    fn change_username(&mut self, new_username: String){
+        self.username = new_username;
+    }
 }
